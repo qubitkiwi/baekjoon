0f020c396df10c61261f8416a8959f65d4c2d912
qubitkiwi <asong1230@gmail.com>
2022-04-17 21:51:49 +0900

    다익스트라 문제

--- 4485.c
+++ 4485.c
@@ -1,51 +1,83 @@
 #include <stdio.h>
 
+int map[126][126];
+int dist[126][126];
+int queue[126*126];
+
+void dijkstra(int N)
+{
+    int f, r, num, max=126*126;
+    int x, y, nx, ny;
+    int move[2][4] = {{1, -1, 0, 0},
+                      {0, 0, 1, -1}};
+
+    f = r =0;
+    
+    dist[0][0] = map[0][0];
+
+    queue[r++] = 0;
+    queue[r++] = 0;
+    num = 2;
+
+    while(num)
+    {
+        x = queue[f++];
+        y = queue[f++];
+        f %= max;
+        num -=2;
+
+        if(x==N-1 && y==N-1)
+            continue;
+
+        for (int i = 0; i < 4; i++)
+        {
+            nx = x + move[0][i];
+            ny = y + move[1][i];
+            if(nx == 0 && ny ==0)
+                continue;
+            if(0<=nx && nx<N && 0<=ny && ny<N)
+            {
+                if(dist[x][y]+map[nx][ny] < dist[nx][ny])
+                {
+                    dist[nx][ny] = dist[x][y]+map[nx][ny];
+
+                    queue[r++] = nx;
+                    queue[r++] = ny;
+                    r %= max;
+                    num += 2;
+                }
+            }   
+        }
+    }
+}
+
+
 int main(void)
 {
     int N, i, j;
-    int map[125][125];
-    int dp[125][125];
+    long  count=0;
 
     while(1)
     {
         scanf("%d", &N);
-        if(!N) break;
+        if(N == 0)
+            break;
+        
+        count++;
 
+        // 입력, 초기화
         for(i=0; i<N; i++)
+        {
             for(j=0; j<N; j++)
             {
                 scanf("%d", &map[i][j]);
-                dp[i][j] = 0;
-            }
-        
-        dp[0][0] = map[0][0];
-
-        for(i=1; i<N; i++)
-        {
-            dp[i][0] = dp[i-1][0] + map[i][0];
-            dp[0][i] = dp[0][i-1] + map[0][i];
-        }
-
-        for(i=1; i<N; i++)
-        {
-            for(j=1; j<N; j++)
-            {
-                dp[i][j] = dp[i][j-1] > dp[i-1][j] ? dp[i-1][j]+map[i][j]: dp[i][j-1]+map[i][j];
+                dist[i][j] = __INT_MAX__;
             }
         }
 
+        dijkstra(N);
         
-
-        for(i=0; i<N; i++)
-        {
-            for(j=0; j<N; j++)
-                printf("%d ", dp[i][j]);
-            printf("\n");
-        }
-
-
-
+        printf("Problem %ld: %d\n", count, dist[N-1][N-1]);
     }
-
     return 0;
 }
