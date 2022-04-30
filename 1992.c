86a7ec3d8a972179cb1b8e2b5ca577b1b3a494fe
qubitkiwi <asong1230@gmail.com>
2022-04-30 11:49:34 +0900

    분할정복

--- 1992.c
+++ 1992.c
@@ -1,53 +1,60 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-
-int rec(int arr[], int N, int s)
+void dfs(char **arr, int N, int x, int y)
 {
-    int i, j;
-    int start[4] = {0, N/2, }
+    int i, j, cnt_0=0;
 
-    if(N==1)
+    for(i=x; i<N+x; i++)
     {
-        return 0;
-    }
-    printf("(");
-
-    for(i=0; i<N/2; i++)
-    {
-        for(j=0; j<N/2; j++)
+        for(j=y; j<N+y; j++)
         {
-            
+            cnt_0 += arr[i][j]-'0';
         }
     }
+
     
 
+    if(cnt_0 == 0)
+    {
+        printf("0");
+    }
+    else if(cnt_0 == N*N)
+    {
+        printf("1");
+    }
+    else
+    {
+        printf("(");
+        dfs(arr, N/2, x, y);
+        dfs(arr, N/2, x, y+N/2);
+        dfs(arr, N/2, x+N/2, y);
+        dfs(arr, N/2, x+N/2, y+N/2);
+        printf(")");
+    }
 
-
-
-
-    printf(")");
+    
 }
 
 
 int main(void)
 {
     int N, i, j;
+    char **arr;
     scanf("%d", &N);
-    int arr[N][N];
+
+    arr = (char **)calloc(N, sizeof(char *));
+    for(i=0; i<N; i++)
+        arr[i] = (char *)calloc(N, sizeof(char));
 
     for(i=0; i<N; i++)
-    {
-        for (j = 0; j < N; j++)
-        {
-            scanf("%d", &arr[i][j]);
-        }
-    }
+        scanf("%s", arr[i]);
 
+    dfs(arr, N, 0, 0);
+    printf("\n");
     
-    
-
-
-
-
+    for(i=0; i<N; i++)
+        free(arr[i]);
+    free(arr);
     return 0;
 }
