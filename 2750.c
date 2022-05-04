d1ef5c22778c5831f9176bc56e017e0bd33d5d9b
qubitkiwi <asong1230@gmail.com>
2022-05-05 06:01:20 +0900

    정렬

--- 2750.c
+++ 2750.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int compar(const void *a,const void *b)
+{
+    int num1 = *(int *)a;
+    int num2 = *(int *)b;
+
+    if(num1<num2)
+        return -1;
+    if(num1>num2)
+        return 1;
+    return 0;
+}
+
 int main(void)
 {
     int N, i, j, last, t;
@@ -10,20 +22,13 @@ int main(void)
     arr = malloc(N*sizeof(int));
 
     for(i=0; i<N; i++)
-    {
         scanf("%d", &arr[i]);
-    }
-
     
-
-
-
-    printf("\n");
+    qsort(arr, N, sizeof(int), compar);
 
     for(i=0; i<N; i++)
-    {
         printf("%d\n", arr[i]);
-    }
 
+    free(arr);
     return 0;
 }
--- 2947.c
+++ 2947.c
@@ -1,37 +1,42 @@
 #include <stdio.h>
 
+void print(int *arr, int N)
+{
+    for (int i = 0; i < N; i++)
+        printf("%d ", arr[i]);
+    printf("\n");    
+}
+
+int chek(int *arr, int N)
+{
+    for (int i = 0; i < N; i++)
+        if(arr[i] > arr[i+1])
+            return 1;
+
+    return 0;
+}
+
 int main(void)
 {
     int i, j, k, t, wood[5];
 
     for(i=0; i<5; i++)
-    {
         scanf("%d", &wood[i]);
+    
+    while(chek(wood, 5))
+    {
+        for(i=0; i<4; i++)
+        {
+            if(wood[i] > wood[i+1])
+            {
+                t = wood[i];
+                wood[i] = wood[i+1];
+                wood[i+1] = t;
+
+                print(wood, 5);
+            }
+        }   
     }
     
-
-    do
-    {
-        for(j=0; j<4; j++)
-        {
-            if(wood[j] > wood[j+1])
-            {
-                t = wood[j];
-                wood[j] = wood[j+1];
-                wood[j+1] = t;
-                break;
-            }
-        }
-
-
-        for(k=0; k<5; k++)
-        {
-            printf("%d ", wood[k]);
-        }
-        printf("\n");
-
-    }while(wood[0] > wood[1] || wood[1] > wood[2] || wood[2] > wood[3] || wood[3] > wood[4] );
-
-
     return 0;
 }
