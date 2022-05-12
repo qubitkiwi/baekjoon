ed8480fa362737d6a3f86a07f59650be8d431cb0
qubitkiwi <asong1230@gmail.com>
2022-05-12 22:39:51 +0900

    수학

--- 13458.c
+++ 13458.c
@@ -3,35 +3,31 @@
 
 int main()
 {
-    int N, B, C;
-    int *A;
-    int i;
-    long long people;
-
+    int *A, B, C, N, i;
+    long long people=0;
 
     scanf("%d", &N);
 
     A = malloc(N*sizeof(int));
     for(i=0; i<N; i++)
-    {
         scanf("%d", &A[i]);
-    }
-
     scanf("%d %d", &B, &C);
 
-    people = N;
+    // people = N;
 
     for(i=0; i<N; i++)
     {
+        A[i] = A[i]- B;
+        people++;
 
-        people += (A[i]-B)/C;
-        
-        if(A[i]%C > 0)
+        if(A[i] > 0)
         {
-            people++;
+            people += A[i]/C;
+            
+            if(A[i]%C > 0)
+                people++;
         }
     }
-
     printf("%lld\n", people);
 
     free(A);
